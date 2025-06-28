da16ca2e3cef44ed818888f285bd97cb5cbdc7b5
CT Reatherford <taoist22@gmail.com>
2025-06-28 12:58:35 -1000

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by CT Reatherford on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
